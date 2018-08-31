afc9d7fabb1fba0f00b2b415a204a87c70a7f924
balathandauthapani <deepika.chinnasamy1998@gmail.com>
2018-08-31 16:39:23 +0530

    first message

---
+++ basics/helloworld.c
@@ -0,0 +1,10 @@
+ Volume in drive F has no label.
+ Volume Serial Number is 0EEB-5DCC
+
+ Directory of F:\labs\basics
+
+31-08-2018  16:04    <DIR>          .
+31-08-2018  16:04    <DIR>          ..
+31-08-2018  16:04                 0 helloworld.c
+               1 File(s)              0 bytes
+               2 Dir(s)  261,344,038,912 bytes free
